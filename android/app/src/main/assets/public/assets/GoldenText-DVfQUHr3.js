294b9ada8ac659b34a69e0f46144a995a952017c
Sohel mohiddin <sohelksk9032899952@gmail.com>
2026-09-09 07:46:20 +0000

    Sync capacitor plugins into android project

--- android/app/src/main/assets/public/assets/GoldenText-DVfQUHr3.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./jsx-runtime-cvnAX2Ol.js";import{t}from"./react-DwkaDh8i.js";t();var n=e();function r({children:e}){return(0,n.jsx)(`span`,{style:{color:`#D4AF37`,textShadow:`0 0 10px rgba(212,175,55,0.6)`,fontWeight:`700`},children:e})}export{r as t};
--- android/app/src/main/assets/public/assets/dist-DaosEApL.js
+++
@@ -1,1 +0,0 @@
-import{o as e,t}from"./jsx-runtime-cvnAX2Ol.js";import{t as n}from"./react-DwkaDh8i.js";var r=e(n(),1);t();var i=Object.defineProperty,a=(e,t)=>i(e,`name`,{value:t,configurable:!0}),o=r.createContext(void 0);function s(e){let t=r.useContext(o);return e||t||`ltr`}a(s,`useDirection`);export{s as t};
